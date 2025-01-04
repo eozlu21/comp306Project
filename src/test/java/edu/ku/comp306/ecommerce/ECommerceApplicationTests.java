0256c6ec96d37bf5d6193b9cb385d6ee9a0e2661
Ege Erdem Özlü <eozlu21@ku.edu.tr>
2025-01-04 22:13:15 +0300

    Initial commit

---
+++ src/test/java/edu/ku/comp306/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ku.comp306.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
