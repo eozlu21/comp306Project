3312540adab5a97c8b1a8304f1baf7ac44d4cfd2
Ceylin <ceylincelik20@ku.edu.tr>
2025-01-13 22:39:23 +0300

    Homepage screen started

---
+++ src/main/java/edu/ku/comp306/ecommerce/controller/HomepageScreenController.java
@@ -0,0 +1,25 @@
+package edu.ku.comp306.ecommerce.controller;
+import edu.ku.comp306.ecommerce.service.ProductService;
+import edu.ku.comp306.ecommerce.entity.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomepageScreenController {
+
+    private final ProductService productService;
+
+    public HomepageScreenController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/homepage")
+    public String getHomePage(Model model) {
+        List<Product> products = productService.getRandomProducts(); // Fetch random 5 products
+        model.addAttribute("products", products);
+        return "homepageScreen";
+    }
+}
